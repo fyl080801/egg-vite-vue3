0690d4717c55d3441653ca8270faf4bfe7c6da57
fengyuanliang3 <fengyuanliang2@jd.com>
2021-02-26 11:20:08 +0800

    fix: 编译问题

--- test/app/controller/home.test.ts
+++ test/app/controller/home.test.ts
@@ -2,8 +2,8 @@ import * as assert from 'assert';
 import { app } from 'egg-mock/bootstrap';
 
 describe('test/app/controller/home.test.ts', () => {
-  it('should GET /', async () => {
-    const result = await app.httpRequest().get('/').expect(200);
-    assert(result.text === 'hi, egg');
+  it('should GET /api', async () => {
+    const result = await app.httpRequest().get('/api').expect(200);
+    assert(result.text === 'api');
   });
 });
