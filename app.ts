7b346119e54bbc2dcd226b8d41f828ed296079d7
fengyuanliang3 <fengyuanliang2@jd.com>
2021-03-12 15:08:36 +0800

    chore: 先去掉开始的数据迁移

--- app.ts
+++ app.ts
@@ -3,10 +3,11 @@ import { Application } from 'egg';
 const startup = (app: Application) => {
   // app.model.define()
   // console.log(app.name);
+  // app.model.sync({
+  //   alter: { drop: false },
+  // });
 
-  app.model.sync({
-    alter: { drop: false },
-  });
+  console.log(app.name);
 };
 
 export default (app: Application) => {
