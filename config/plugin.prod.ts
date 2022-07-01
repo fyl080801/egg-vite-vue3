6fb9398b8639cc88bc624e81e28598d07723d617
fengyuanliang3 <fengyuanliang2@jd.com>
2022-07-01 12:27:13 +0800

    fix: 静态资源构建问题

---
+++ config/plugin.prod.ts
@@ -0,0 +1,9 @@
+import { EggPlugin } from 'egg';
+
+const plugin: EggPlugin = {
+  '@egglib/statics-vite': {
+    enable: false,
+  },
+};
+
+export default plugin;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import legacy from '@vitejs/plugin-legacy';
+import { name } from './package.json';
 
 export default defineConfig({
+  base: `/${name}/`,
   build: {
     manifest: true,
   },
